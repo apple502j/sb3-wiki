aaf900ae594c3a54f912c9e927edbffc2bbb3976
apple502j <33279053+apple502j@users.noreply.github.com>
2018-03-10 19:15:10 +0900

    Create run_s3blocks.js
    https://github.com/tjvr/wiki-scratchblocks/blob/master/run_scratchblocks.js
    From here

---
+++ run_s3blocks.js
@@ -0,0 +1,1 @@
+scratchblocks.renderMatching("pre.blocks3, code.blocks3", {languages: ["en", "de", "es", "el", "nl", "pt", "zh_CN", "tr", "nb", "ko", "it", "id", "ru", "ca", "ja", "pl", "he", "fr"],});
