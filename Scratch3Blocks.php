c99529f9d85f6684048c6b7efb3cbfb772185653
apple502j <33279053+apple502j@users.noreply.github.com>
2018-03-10 19:08:44 +0900

    Create Scratch3Blocks.php
    https://github.com/tjvr/wiki-scratchblocks/blob/master/ScratchBlocks.php
    From this code and modified

---
+++ Scratch3Blocks.php
@@ -0,0 +1,55 @@
+<?php
+/*
+ * ScratchBlocks extension for MediaWiki
+ * Renders <scratchblocks> tags to shiny scratch blocks
+ *
+ * Copyright 2013, Tim Radvan
+ * Copyright 2018, Apple502j
+ * MIT Licensed
+ * http://opensource.org/licenses/MIT
+ *
+ * Includes s3blocks
+ * https://github.com/s3blocks/s3blocks
+ *
+ */
+
+
+if (!defined('MEDIAWIKI')) {
+    die();
+}
+
+require_once __DIR__ . "/Scratch3BlocksHooks.php";
+
+// Hooks
+
+$wgExtensionFunctions[] = 'Scratch3BlocksHooks::sbSetup';
+$wgHooks['ParserFirstCallInit'][] = 'Scratch3BlocksHooks::sbParserInit';
+
+
+// Define resources
+
+$wgResourceModules['ext.scratch3Blocks'] = array(
+    'scripts' => array(
+        's3blocks/src/scratchblocks.js',
+        's3blocks/src/translations.js',
+        'run_s3blocks.js',
+    ),
+
+    'styles' => '/inline.css',
+
+    // jQuery is loaded anyway
+    'dependencies' => array(),
+
+    // Where the files are
+    'localBasePath' => __DIR__,
+    'remoteExtPath' => 'ScratchBlocks'
+);
+
+$wgExtensionCredits['parserhook'][] = array(
+    'name' => "Scratch3blocks",           // Name of extension - string
+    'description' => "This plugin can add Scratch 3.0's block in the wiki.",
+    'version' => 1.0,
+    'author' => ["ErnieParke","blob8108","tjvr","NitroCipher","apple502j"],
+    'url' => "https://github.com/apple502j/sb3-wiki",
+    'license-name' => "MIT",
+);
